b6ceafa51ef284133548ec1cea87c157bb99946c
lyutok <48644325+lyutok@users.noreply.github.com>
2026-03-08 16:53:38 +0000

    Make butterflies 20% smaller

--- src/components/PlanetScene.tsx
+++ src/components/PlanetScene.tsx
@@ -794,7 +794,7 @@ function Butterfly({ pos, index }: { pos:[number,number,number]; index: number }
   });
 
   return (
-    <group ref={groupRef} scale={0.5}>
+    <group ref={groupRef} scale={0.4}>
       {/* wing flap pivot */}
       <group ref={wingRef}>
         <ButterflyWing side={1}  col={col} spotCol={spotCol} />

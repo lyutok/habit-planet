30484e0b798a98856804038f110af56c8c066632
lyutok <48644325+lyutok@users.noreply.github.com>
2026-03-08 13:14:22 +0000

    Reset all data in dev panel

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -38,6 +38,7 @@ const Index = () => {
     getCurrentStreak,
     getTodayCount,
     simulateStreak,
+    resetAll,
   } = useHabits({ getToday });
 
   const [showModal, setShowModal] = useState(false);

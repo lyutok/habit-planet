f0e86a2da83887461a626da20b7791c2f7eefdd0
lyutok <48644325+lyutok@users.noreply.github.com>
2026-03-08 16:42:31 +0000

    Changes

--- src/components/PlanetScene.tsx
+++ src/components/PlanetScene.tsx
@@ -794,7 +794,7 @@ function Butterfly({ pos, index }: { pos:[number,number,number]; index: number }
   });
 
   return (
-    <group ref={groupRef} scale={0.65}>
+    <group ref={groupRef} scale={0.5}>
       {/* wing flap pivot */}
       <group ref={wingRef}>
         <ButterflyWing side={1}  col={col} spotCol={spotCol} />
